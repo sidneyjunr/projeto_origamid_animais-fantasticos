780983f25bacb7abea85c1b1faf85cc52c86ee2b
Sidney <sidneyferreirabaptistajunior3@gmail.com>
2024-05-09 14:17:09 -0300

    javascript implementado até o scroll suave

--- script.js
+++ script.js
@@ -1,19 +1,71 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
-if(tabMenu.length && tabContent.length){    
-    
-    tabContent[0].classList.add('ativo')
-    
-    function activeTab(index){
-        tabContent.forEach((section)=>{
-            section.classList.remove('ativo')
+function initTabNav(){
+
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
+    if(tabMenu.length && tabContent.length){    
+        
+        tabContent[0].classList.add('ativo')
+        
+        function activeTab(index){
+            tabContent.forEach((section)=>{
+                section.classList.remove('ativo')
+            })
+            tabContent[index].classList.add('ativo')
+        }
+        
+        tabMenu.forEach((item,index)=>{
+            item.addEventListener('click',()=>{
+                activeTab(index)
+            })
         })
-        tabContent[index].classList.add('ativo')
     }
-    
-    tabMenu.forEach((item,index)=>{
-        item.addEventListener('click',()=>{
-            activeTab(index)
-        })
-    })
 }
+initTabNav()
+
+function initAccordion(){    
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    const activeClass = 'ativo'
+
+    if(accordionList.length){
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+        
+        function activeAccordion(){
+            this.classList.toggle(activeClass)
+            this.nextElementSibling.classList.toggle(activeClass)
+        }
+        
+        accordionList.forEach((item)=>{
+            item.addEventListener('click', activeAccordion)
+        })
+    }
+}
+initAccordion()
+
+function initScrollSuave(){
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+    function scrollToSection(e){
+        e.preventDefault()
+        const href = e.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        
+        // FORMA ALTERNATIVA
+        // const topo = section.offsetTop
+        // window.scrollTo({
+            //     top: topo,
+            //     behavior:"smooth"
+            // })
+        }
+        
+        linksInternos.forEach((i)=>{
+            i.addEventListener('click',scrollToSection)
+        })
+}
+initScrollSuave()
